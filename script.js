1d0a750ff3eb6a535faaba3b0b98a0c0d40dfc55
Pushapak Kumar <pp281997@gmail.com>
2021-02-24 13:46:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+var date = new Date();
+console.log(date);
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log(hr, min, sec);
+
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
+
+function runClock() {
+
+    hrPosition = hrPosition + (30 / 3600);
+    minPosition = minPosition + (1 / 60) * 6;
+    secPosition = secPosition + 360 / 60;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runClock, 1000);
